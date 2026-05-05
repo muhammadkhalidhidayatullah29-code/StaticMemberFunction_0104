ea0b6aabc5b179f4748e002edc0d68dba84b3b0e
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-05-05 18:57:57 +0700

    menambah member int id, string nama, void setID, void printAll, static void setNim, static int getNim dan constructor Mahasiswa dengan parameter pnama

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -7,7 +7,21 @@ class Mahasiswa {
 private:
     static int nim;
 public:
-    
-    
-        
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();  
+
+    static void setNim(int pnim) {
+        nim = pnim;
+    }; 
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
 };
