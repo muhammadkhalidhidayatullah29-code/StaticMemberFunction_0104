b232f572fa0cd4695808cfc4aefbe88006da61c8
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-05-05 19:39:52 +0700

    membuat fungsi utama int main

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,36 @@ public:
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa :: mahasiswa() {
+}
+
+mahasiswa :: mahasiswa(int iNim) {
+    nim = iNim; 
+}
+
+mahasiswa :: mahasiswa(string iNama) {
+    nama = iNama; 
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama) {
+    nim = iNim; 
+    nama = iNama;
+}
+
+void mahasiswa :: cetak() {
+    cout << endl << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1 (102030);                  
+    mahasiswa mhs2 ("khalid");                  
+    mahasiswa mhs3 (010203, "Rifqi");          
+
+    cout <<"Mahasiswa 1:"; mhs1.cetak();
+    cout <<"Mahasiswa 2:"; mhs2.cetak();
+    cout <<"Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
