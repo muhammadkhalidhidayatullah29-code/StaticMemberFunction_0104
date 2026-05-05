98a3d2fd0bd6e29829c61134d1146b3e164bce7c
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-05-05 18:43:06 +0700

    membuat fungsi utama int main

--- staticmember.cpp
+++ staticmember.cpp
@@ -15,3 +15,29 @@ public:
         setID();
     } 
 };
+
+int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID() {
+    id = ++nim;
+};
+
+void Mahasiswa::printAll() {
+    cout << "ID: " << id << endl;
+    cout << "Nama: " << nama << endl;
+    cout << endl;
+};
+
+int main() {
+    Mahasiswa mhs1("Khalid");
+    Mahasiswa mhs2("Rifqi");
+    Mahasiswa mhs3("Aisyah");
+    Mahasiswa mhs4("Hanifah");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
+}
